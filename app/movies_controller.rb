80494b176d25c2eb9ddbfd953195c1db915c208f
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-22 03:59:10 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,10 +29,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    m = Movie.new
     m.title = title
     m.release_date = release_date
-    m.save
   end
 end
 
